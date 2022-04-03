cefd36b618c4443931e7fb6815f77198e03afd43
axdibble <axdibble@ufl.edu>
2022-04-03 12:56:17 -0400

    added express backend

---
+++ client/components/Example.js
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+import React from 'react'
+const Example = () => {
+    return (
+        <Text style={styles.text}>Knot &lt;3</Text>
+    )
+}
+
+export default Example
+
+
+const styles = StyleSheet.create({
+    text: {
+      fontSize: 20
+    }
+})
+  
--- client/screens/HomeScreen.js
+++ client/screens/HomeScreen.js
@@ -1,10 +1,11 @@
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
 import React from 'react'
+import Example from '../components/Example.js'
 
 const HomeScreen = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.text}>Knot &lt;3</Text>
+      <Example />
     </SafeAreaView>
   )
 }
@@ -12,11 +13,12 @@ const HomeScreen = () => {
 export default HomeScreen
 
 const styles = StyleSheet.create({
-  text: {
-    fontSize: '2rem'
-  },
-  container: {
+container: {
+    flex: 1,
+    display: 'flex',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    backgroundColor: '#c4c4c4',
+    textAlign: 'center'
   }
 })
